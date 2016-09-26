1bb4903bea22d6b8ec6d50b319910dfb55ecada7
Johnny Hauser <johnnyhauser@gmail.com>
2016-09-25 22:03:52 -0500

    parser test

--- test/index.js
+++ test/index.js
@@ -1,14 +1,22 @@
 const test = require('tape')
 const parser = require('@m59/tap-parser')
 const filter = require('../')
+const through = require('throo')
+
+test('parser stream', t => {
+  t.plan(1)
+  const p = parser()
+  p.on('data', data => {
+    t.deepEqual(data, {parsed: {end: 2, start: 1}, type: 'plan', value: '1..2'})
+  })
+  p.write('1..2\n')
+})
 
 test('filters out "plan" when given "test" type', t => {
   t.plan(1)
-  const stream = parser().pipe(filter(['test']))
-  stream.on('data', data => {
-    stream.end()
-    t.equal(data, 'ok 1 stuff')
-  })
-  stream.write('1..2')
-  stream.write('ok 1 stuff')
+  const p = parser().pipe(filter(['test'])).pipe(through((push, chunk, enc, cb) => {
+    t.equal(String(chunk), 'ok 1 stuff\n')
+  }))
+  p.write('1..2\n')
+  p.write('ok 1 stuff\n')
 })
